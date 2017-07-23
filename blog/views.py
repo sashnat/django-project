03a6b5aa776a04c3e8f8bae423101af14378e7c2
sashnat <sashnat@yahoo.com>
2017-07-23 23:48:42 +0300

    add css html

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
+# Точка перед models означает текущую директорию или текущее приложение
+# Поскольку views.py и models.py находятся в одной директории,
+# мы можем использовать точку . и имя файла (без расширения .py).
+# Затем мы импортируем модель (Post).
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+# мы создали переменную для QuerySet: posts
+    return render(request, 'blog/post_list.html', {'posts': posts})
