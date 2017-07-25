fbb8ecd111c3f412df4f4fdca5dbdaec464e88a9
sashnat <sashnat@yahoo.com>
2017-07-25 14:26:42 +0300

    extension

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 # Точка перед models означает текущую директорию или текущее приложение
 # Поскольку views.py и models.py находятся в одной директории,
 # мы можем использовать точку . и имя файла (без расширения .py).
 # Затем мы импортируем модель (Post).
 def post_list(request):
-    
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-# мы создали переменную для QuerySet: posts
+    # мы создали переменную для QuerySet: posts
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detai.html', {'post': post})
